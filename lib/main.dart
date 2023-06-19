ce9debe9131f6ccbe9d8ff194654ab3212a22fbf
AndresLince <lynxdarmatron@gmail.com>
2023-06-19 14:37:35 -0500

    Change image asset

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,17 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget{
+class DicePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Row(
       children: [
-        Expanded(child: Image(
-          image: AssetImage(
-            'images/dice1.png'
-          ),
-        ))
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        )
       ],
     );
   }
